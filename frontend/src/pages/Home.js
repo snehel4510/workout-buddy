29fca9cff234037763c442b359074c4a67f9b71a
Snehel <sahilrps31@gmail.com>
2022-07-27 14:31:43 +0530

    using the workout global state instead of local

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,21 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const Home = () => {
 
-    const [workouts, setWorkouts] = useState(null);
+    // const [workouts, setWorkouts] = useState(null);
+    const {workouts, dispatch} = useWorkoutsContext();
 
     useEffect(() => {
         const fetchWorkouts = async () => {
             const response = await fetch('/api/workouts/');
             const data = await response.json();
             if(response.ok){
-                setWorkouts(data);
+                // setWorkouts(data);
+                dispatch({type: "SET_WORKOUTS", payload: data});
             }
         }
         fetchWorkouts();
-    }, [])
+    }, [dispatch])
 
     return ( 
         <div className="home">
