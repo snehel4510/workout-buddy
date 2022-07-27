70ace8dde623330d8005ad53571c5b06f2d4ea9d
Snehel <sahilrps31@gmail.com>
2022-07-27 12:50:03 +0530

    different route's pages

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+
+const Home = () => {
+
+    const [workouts, setWorkouts] = useState(null);
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts/');
+            const data = await response.json();
+            if(response.ok){
+                setWorkouts(data);
+            }
+        }
+        fetchWorkouts();
+    }, [])
+
+    return ( 
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map(workout => (
+                    <WorkoutDetails key={workout._id} workout={workout}/>
+                ))}
+            </div>
+            <WorkoutForm />
+        </div>
+
+     );
+}
+ 
+export default Home;
